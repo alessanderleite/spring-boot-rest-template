8dce01cef3be9db5ad39f53a98b81fd5546a8542
Alessander <alessander.logistica@gmail.com>
2020-05-29 23:09:48 -0300

    create a config class and call CommandLineRunner and RestTemplate

---
+++ src/main/java/com/example/app/config/QuoteConfig.java
@@ -0,0 +1,31 @@
+package com.example.app.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.app.domain.Quote;
+
+@Configuration
+public class QuoteConfig {
+
+	private static final Logger log = LoggerFactory.getLogger(QuoteConfig.class);
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", 
+					Quote.class);
+			log.info(quote.toString());
+		};
+	}
+}
