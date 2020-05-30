de86188775dd1f0a1f9c16e20f6ddff1d3c3f465
Alessander <alessander.logistica@gmail.com>
2020-05-29 23:21:38 -0300

    config client url

--- src/main/java/com/example/app/config/QuoteConfig.java
+++ src/main/java/com/example/app/config/QuoteConfig.java
@@ -2,6 +2,7 @@ package com.example.app.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,16 +16,19 @@ public class QuoteConfig {
 
 	private static final Logger log = LoggerFactory.getLogger(QuoteConfig.class);
 	
+	@Value("${client.url}")
+	private String url;
+	
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
 	
+	
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
-			Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", 
-					Quote.class);
+			Quote quote = restTemplate.getForObject(url, Quote.class);
 			log.info(quote.toString());
 		};
 	}
